4eee9795f607b9ca2ec45d570d651cb11839b23b
UberActivist <UberActivist@gmail.com>
2015-12-27 17:33:52 -0600

    Reworked commands and added args. This part is finally complete.

--- main.js
+++ main.js
@@ -15,10 +15,9 @@ function statusReport(report) {
 }
 
 var commands = {
-    "ping": function(channelID, user, userID, message) {
-		var msplit = message.slice(1).toLowerCase().split(" ");
+    "ping": function(channelID, user, userID, message, msplit) {
 		var args = {
-			"justin":function() {
+			"justin": function() {
 				statusReport("INFO: Responded to ping (type: justin) from " + user);
 				bot.sendMessage({to: channelID, message: "Justinisanoob!"});
 			}
@@ -30,15 +29,56 @@ var commands = {
 			statusReport("WARN: " + user + " gave invalid argument " + msplit[1] + " for command ping.");
 			bot.sendMessage({to:channelID, message: "Invalid Arguments."});
 		}
-		else if (msplit[1] == undefined) {
+		else {
 			statusReport("INFO: Responded to ping from " + user);
 			bot.sendMessage({to:channelID, message: "Pong!"});
 		}
-		else {
-			statusReport("WARN: " + user + " gave command " + message + " and something wrong happened!")
+    },
+	"help": function(channelID, user, userID, message, msplit) {
+		var args = {
+			"help": function() {
+				statusReport("INFO: Responded to help with argument help from " + user);
+				bot.sendMessage({to: userID, message: "&help <command> - Gives a list of commands, and alternatively the specifics of a single command if an argument is given."});
+			},
+			"rtd": function() {
+				statusReport("INFO: Responded to help with argument rtd from " + user);
+				bot.sendMessage({to: userID, message: "&rtd - Rolls the dice."})
+			},
+			"version": function() {
+				statusReport("INFO: Responded to help with argument version from " + user);
+				bot.sendMessage({to: userID, message: "&version - Lists the current version of the bot."});
+			}
 		}
-    }
-};
+		if (msplit[1] in args) {
+			args[msplit[1]]();
+		}
+		else if (msplit[1] != undefined) {
+			statusReport("WARN: " + user + " gave invalid argument " + msplit[1] + " for command help.");
+			bot.sendMessage({to: userID, message: "Invalid argument. Correct use: &help <command>"})
+		}
+		else {
+			bot.sendMessage({to: userID, message: "Commands: \n &help <command> - Lists the commands. (Sends in a Direct Message) \n &ping <argument> - Used to check your connection. \n &rtd - Roll the Dice! \n &version - Lists the version of the bot."});
+			statusReport("INFO: Responded to Help from " + user + ".");
+		}
+	},
+	"rtd": function (channelID, user, userID, message, msplit) {
+		bot.sendMessage({to: channelID, message: "*rolls the dice*"});
+		statusReport("INFO: Rolled the dice for " + user + ".")
+		bot.simulateTyping(channelID, function() {
+			var die1 = random.integer(1,6)
+			var die2 = random.integer(1,6)
+			var total = die1 + die2
+			bot.setPresence({game: "Roll the Dice"});
+			statusReport("INFO: Dice Roll, waiting 3 seconds.")
+			setTimeout(function() { 
+				bot.sendMessage({to: channelID, message: "@" + user + " : I rolled two 6-sided dice. Die one is a " + die1 + " and die two is a " + die2 +". I rolled a total of " + total }); 
+				statusReport("INFO: Reported dice roll to " + user + ".");
+				bot.setPresence({game: null});
+				statusReport("INFO: Dice Roll complete.")
+			}, 3000);
+		});
+	}
+}
 
 var tempSettings = {quietmode:false}
 var dono = ["+Jack","Justin","Kyle","Luke"]
@@ -67,47 +107,16 @@ bot.on('message', function(user, userID, channelID, message, rawEvent) {
 	var msplit = message.slice(1).toLowerCase().split(" ");
 	if (userID != bot.id) { //filter out bot's own messages
 		if (message.slice(0,1) == "&") { //look for the command operator
-			if (bot.servers[server]) {
-				statusReport("INTERACTION: " + bot.servers[server].name + " in #" + bot.servers[server].channels[channelID].name + " - " + user + ": " + message); //Log messages to disk.
-				if (msplit[0] in commands) {
-					commands[msplit[0]](channelID, user, userID, message);
-				}
-				else if (message.slice(1) == "help") {
-					bot.sendMessage({to: userID, message: "Commands: \n help - Lists the commands. (Sends in a Direct Message) \n ping - Used to check your connection. \n Use the & operator to use a command."});
-					statusReport("INFO: Responded to Help from " + user + ".");
-				}
-				else if (message.slice(1) == "rtd") {
-					bot.sendMessage({to: channelID, message: "*rolls the dice*"});
-					statusReport("INFO: Rolled the dice for " + user + ".")
-					bot.simulateTyping(channelID, function() {
-						var die1 = random.integer(1,6)
-						var die2 = random.integer(1,6)
-						var total = die1 + die2
-						bot.setPresence({game: "Roll the Dice"});
-						statusReport("INFO: Dice Roll, waiting 3 seconds.")
-						setTimeout(function() { 
-						bot.sendMessage({to: channelID, message: "@" + user + " : I rolled two 6-sided dice. Die one is a " + die1 + " and die two is a " + die2 +". I rolled a total of " + total }); 
-						statusReport("INFO: Reported dice roll to " + user + ".");
-						bot.setPresence({game: null});
-						statusReport("INFO: Dice Roll complete.")
-						}, 3000);
-					});
-				}
-				else if (message.slice(1) == "version") {
-					//NYI
-				}
-				else {
-					/*bot.sendMessage({
-					to: channelID,
-					message: "@" + user + " : Unknown Command."
-					}); */
-					statusReport("WARNING: " + user + " used unknown command " + message)
-				}
+			statusReport("INTERACTION: " + user + ": " + message); //Log messages to disk.
+			if (msplit[0] in commands) {
+				commands[msplit[0]](channelID, user, userID, message, msplit);
 			}
 			else {
-				statusReport("WARNING: " + user + " tried to enter a command via Direct Message. This functionality is currently in development.");
-				bot.sendMessage({to: userID,
-				message: "Please only send commands in a valid channel. Direct messages do not yet work. Please visit https://discord.gg/0imlVmAZ4lWaxQKv to send commands without disturbing others."})
+				/*bot.sendMessage({
+				to: channelID,
+				message: "@" + user + " : Unknown Command."
+				}); */
+				statusReport("WARNING: " + user + " used unknown command " + message)
 			}
 		}
 	}
